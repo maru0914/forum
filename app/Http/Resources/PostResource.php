36f9f01b686b0d994572a4da433c448dbedea063
emmaru <littleoduozu@gmail.com>
2024-05-04 16:42:28 +0900

    APIリソースの導入

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Post
+ */
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => UserResource::make($this->user),
+            'title' => $this->title,
+            'body' => $this->body,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
